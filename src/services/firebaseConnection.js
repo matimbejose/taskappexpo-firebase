71b99854333935dc92137ac91412e7b0d9e6c76e
José Justino Matimbe <81678247+matimbejose@users.noreply.github.com>
2023-07-16 11:50:05 +0200

    Update firebaseConnection.js

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -4,13 +4,7 @@ import 'firebase/compat/database'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKmfTZC_1jM5SVHdsYCtRG7E_6rfxMs84",
-    authDomain: "apptarefas-a3de6.firebaseapp.com",
-    projectId: "apptarefas-a3de6",
-    storageBucket: "apptarefas-a3de6.appspot.com",
-    messagingSenderId: "906256995359",
-    appId: "1:906256995359:web:3da66f96f5f7156410712a",
-    measurementId: "G-2ZJVXDBK4R"
+
   };
   
 
